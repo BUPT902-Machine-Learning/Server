418b14a9d1736fa1363615e1187b25d58dec5961
SimonYi <694055593@qq.com>
2019-06-05 14:18:40 +0800

    new version 2.0

--- Machinelearning/cooperation/models.py
+++ Machinelearning/cooperation/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from classInfo.models import Teacher
 from textInteraction.models import TextModelBasicInfo
 
+from users.models import Users
+from django.contrib.auth.models import User
+
+
+def content_save_path(instance, filename):
+    path = str(instance.model_name.en_name) + "/" + str(instance.label) + "/" + filename
+    print(path)
+    return path
+
 
 class CooperationModels(models.Model):
     # TRAIN_STATUS_CHOICES = (
@@ -36,3 +45,55 @@ class CooperationData(models.Model):
     contents = models.TextField(max_length=65535)
     data_create = models.DateTimeField(auto_now_add=True)
     data_update = models.DateTimeField(auto_now=True)
+
+
+class CooperationImageModels(models.Model):
+    DELETE_STATUS_CHOICES = (
+        (1, "已删除"),
+        (0, "未删除")
+    )
+
+    MODEL_TYPE_CHOICES = (
+        (1, "普通模型"),
+        (0, "合作模型")
+    )
+
+    TRAIN_STATUS_CHOICES = (
+        (3, "已训练"),
+        (2, "训练中"),
+        (1, "已发布"),
+        (0, "未发布"),
+    )
+
+    cn_name = models.CharField(max_length=20, null=True)
+    en_name = models.CharField(max_length=20, primary_key=True)
+    user_belong = models.ForeignKey(Users)
+    labels = models.TextField(max_length=65535, null=True)
+    algorithm = models.CharField(max_length=10, default="CNN-SVM")
+    accuracy = models.FloatField(null=True)
+    delete_status = models.IntegerField(verbose_name="删除状态", choices=DELETE_STATUS_CHOICES, default=0)
+    train_status = models.IntegerField(verbose_name="训练状态", choices=TRAIN_STATUS_CHOICES, default=0)
+    model_type = models.IntegerField(verbose_name="模型类型", choices=MODEL_TYPE_CHOICES, default=1)
+    data_create = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
+
+
+class CooperationImageTrainData(models.Model):
+    DELETE_STATUS_CHOICES = (
+        (1, "已删除"),
+        (0, "未删除")
+    )
+    user_belong = models.ForeignKey(Users)
+    model_name = models.ForeignKey(CooperationImageModels)
+    image_name = models.TextField(max_length=65535)
+    content = models.ImageField(null=True, upload_to=content_save_path)
+    label = models.TextField(max_length=65535)
+    delete_status = models.IntegerField(verbose_name="删除状态", choices=DELETE_STATUS_CHOICES, default=0)
+    data_create = models.DateTimeField(auto_now_add=True)
+    data_update = models.DateTimeField(auto_now=True)
+
+
+class CooperationImageLabelMap(models.Model):
+    model_name = models.ForeignKey(CooperationImageModels)
+    real_labels = models.TextField(max_length=65535)
+    train_labels = models.TextField(max_length=65535)
--- Machinelearning/cooperation/urls.py
+++ Machinelearning/cooperation/urls.py
@@ -9,5 +9,19 @@ urlpatterns = [
     url(r'^pushData/$', cooperation_views.API.push_data),
     url(r'^trainModel/$', cooperation_views.API.train_model),
     url(r'^ifTrain/$', cooperation_views.API.if_train),
-    url(r'^deleteModel/$', cooperation_views.API.delete_model)
+    url(r'^deleteModel/$', cooperation_views.API.delete_model),
+
+    # image cooperation
+    url(r'^createImageModel/$', cooperation_views.API.create_image_model),
+    url(r'^stuGetCreateImageModel/$', cooperation_views.API.stu_get_create_image_model),
+    url(r'^teachGetCreateImageModel/$', cooperation_views.API.teach_get_create_image_model),
+    url(r'^getImageModelData/$', cooperation_views.API.get_image_model_data),
+    url(r'^pushImageData/$', cooperation_views.API.push_image_data),
+    url(r'^popImageData/$', cooperation_views.API.pop_image_data),
+    url(r'^trainImageModel/$', cooperation_views.API.train_image_model),
+    url(r'^ifImageTrain/$', cooperation_views.API.if_image_train),
+    url(r'^deleteImageModel/$', cooperation_views.API.delete_image_model),
+    url(r'^deleteImageLabel/$', cooperation_views.API.delete_image_label),
+    url(r'^testImageModel/$', cooperation_views.API.test_image_model),
+
 ]
