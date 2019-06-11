6d1bda1a1f80154f9754524b922df136de277be5
SimonYi <694055593@qq.com>
2019-06-11 14:40:12 +0800

    get debug

--- Machinelearning/Machinelearning/production_settings.py
+++ Machinelearning/Machinelearning/production_settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r=wd$&&tnj#j$5n#yd57%(v-)z+lmw*nlza5&8a9qzfb32g)24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'werkzeug_debugger_runserver',
     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
